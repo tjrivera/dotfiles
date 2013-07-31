0c0067a1b609898794490c863e8250850e5a3080
Rivera <riverat2@542696d5303d.chop.edu>
2013-07-31 08:36:53 -0400

    Update bootstrap to allow forced update (zsh compatible)

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,13 +6,13 @@ function doIt() {
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
 	source ~/.zshrc
 }
-#if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt
-#else
-#	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-#	echo
-#	if [[ $REPLY =~ ^[Yy]$ ]]; then
-#		doIt
-#	fi
-#fi
+if [[ "$1" == "--force" || "$1" == "-f" ]]; then
+    doIt
+else
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+    echo
+        if [[ $REPLY =~ ^[Yy]$ ]]; then
+		    doIt
+	    fi
+fi
 unset doIt
